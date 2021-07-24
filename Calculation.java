1c5ae4f1ceb3d3e35eee8e6ef3d91cdf3212e16f
David Magare <david.o.magare@gmail.com>
2021-07-24 17:44:27 -0500

    My first commit

--- Calculation.java
+++ Calculation.java
@@ -1,5 +1,6 @@
-import java.lang.*;
-public class calculation{
+//This is only a simple program, with more enhencements coming.
+//This program only calls one method to calculate a factorial.
+public class Calculation{
 	void fact(int n){
 	int fact = 1;
 	for(int i=1; i<n; i++){
@@ -9,7 +10,7 @@ public class calculation{
        }
 
 public static void  main(String[] args){
-calculation num= new calculation();
-	num.fact(10);
+Calculation num= new Calculation();
+	num.fact(20);
 	}
 }
